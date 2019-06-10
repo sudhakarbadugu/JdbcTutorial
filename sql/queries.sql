88a81a3fac963554738ce4ed41f1acb80f1db001
murali <badugu.murali1993@gmail.com>
2019-06-10 09:19:05 +0530

    jdbc

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,4 +1,21 @@
 -- queries for stock_user
 
 
+insert into stock_user values(107, '2-4-2019', 'Suma', 'Suma', '2-4-2019');
+insert into stock_user values(108, '2-4-2019', 'Suma pushpa', 'Suma', '2-4-2019');
+insert into stock_user values(109, '2-4-2019', 'Suda', 'Suma', '2-4-2019');
+insert into stock_user values(110, '2-4-2019', 'Suma', 'Suma', '2-4-2019');
+
+-- Update all rows
+update stock_user set username = 'Suda';
+ 
+ 
+ -- update specific row
+ update stock_user set username = 'Suda' where username = 'Sudha';
+
+-- delete all rows
+delete from stock_user;
+
+-- delet specific row
+delete from stock_user where username = 'Badugu';
 -- queries for stock_alerts

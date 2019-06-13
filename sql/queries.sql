c0f3be9293d385a84e618ca892f4e28192bfd2ab
murali <badugu.murali1993@gmail.com>
2019-06-13 09:11:31 +0530

    jdbc

--- sql/queries.sql
+++ sql/queries.sql
@@ -11,7 +11,7 @@ update stock_user set username = 'Suda';
  
  
  -- update specific row
- update stock_user set username = 'Suda' where username = 'Sudha';
+update stock_user set username = 'Sudha' where username = 'Suda';
 
 -- delete all rows
 delete from stock_user;
@@ -19,3 +19,10 @@ delete from stock_user;
 -- delet specific row
 delete from stock_user where username = 'Badugu';
 -- queries for stock_alerts
+
+
+create table order_details(order_id number, item_name varchar2(250), quantity number, price number);
+create table order_payment(paymet_id number, order_id number, payment_status varchar2(6));
+
+insert into order_details values(1, 'Lenovo Laptop', 1, 30000);
+insert into order_payment values(1, 1, 'fail');
